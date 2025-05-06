32f40d9d654bff42c499a4d0fc3eead7a7008820
Jeeveneish <jeeveneish@gmail.com>
2025-05-06 19:19:20 +0800

    added nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import {connectDB} from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => {
-    console.log("Server started at http://localhost:5000");
+app.get('/', (req, res) => {
+  res.send('Server is ready YAY!');
 });
+
+app.listen(5000, () => {
+    connectDB();
+  console.log('Server started at http://localhost:5000');
+});
+
